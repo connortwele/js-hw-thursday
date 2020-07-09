50de860716d18326d06b88dd8c5ef2f080afb564
Connor Twele <connortwele@gmail.com>
2020-07-09 16:39:44 -0400

    first push

---
+++ app.js
@@ -0,0 +1,29 @@
+var arrayOne = []
+
+for (var i = 1; i <= 10; i++){
+    arrayOne.push(i);
+}
+console.log(arrayOne)
+
+var arrayTwo = []
+
+var i = 1;
+while (i <= 10){
+    arrayTwo.push(i);
+    i++;
+};
+
+console.log(arrayTwo)
+
+var arrayThree = []
+for (var i = 11; i <= 21; i += 2){
+    arrayThree.push(i);
+}
+
+console.log(arrayThree);
+
+var arrayFour = []
+for (var i = 15; i > 0; i -=2){
+    arrayFour.push(i);
+}
+console.log(arrayFour)
